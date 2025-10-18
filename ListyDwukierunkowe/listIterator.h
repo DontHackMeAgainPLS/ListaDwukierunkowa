1dfad8b2e35051e561d63b124b9e0297e0bcb258
Dominik Zieliński <z.dominik2826@gmail.com>
2025-10-18 14:47:17 +0200

    Dodanie kodu iteratora, dla list dwukierunkowych

--- ListyDwukierunkowe/listIterator.h
+++ ListyDwukierunkowe/listIterator.h
@@ -1,6 +1,33 @@
 #ifndef ITERATOR_H
 #define ITERATOR_H
 
+class listIterator {
+private:
+	node* current;
+public:
+	explicit listIterator(node* start) :current(start) {}
+
+	bool hasNext() const {
+		return current != nullptr;
+	}
+
+	bool hasPrevious() const {
+		return previous != nullptr;
+	}
+
+	int next() {
+		int value = current->data;
+		current = current->next;
+		return value;
+	}
+
+	int previous() {
+		int value = current->data;
+		current = current->previous;
+		return value;
+	}
 
 
-#endif // !ITERATOR_H
+};
+
+#endif // !ITERATOR_H 
